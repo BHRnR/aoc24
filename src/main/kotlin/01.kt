c636ae45fa132b46f68178eec909b11889f7a1d1
Hacker, Benjamin <benjamin.hacker@adesso.de>
2024-12-02 12:23:09 +0100

    Create project, solve first day

---
+++ src/main/kotlin/01.kt
@@ -0,0 +1,21 @@
+import java.io.File
+import kotlin.math.abs
+
+fun lists(): Pair<List<Int>, List<Int>> {
+    val (listA, listB) = File("src/main/resources/input1").readLines()
+        .map {
+            val parts = it.split("   ").map(String::toInt)
+            parts[0] to parts[1]
+        }
+        .unzip()
+
+    return listA.sorted() to listB.sorted()
+}
+
+fun firstA(): Int {
+    return lists().first.zip(lists().second).sumOf { (a, b) -> abs(a - b) }
+}
+
+fun secondB(): Int {
+    return lists().first.sumOf { it * lists().second.count { second -> second == it } }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val start = java.util.Date().time
+    println(secondB())
+    val end = java.util.Date().time
+    println("Took ${end - start}ms.")
+}
