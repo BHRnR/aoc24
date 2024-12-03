b58baeedcfe0ede98ed2a52d5b2c3d15064f2540
Hacker, Benjamin <benjamin.hacker@adesso.de>
2024-12-03 10:42:00 +0100

    Solve day 3

---
+++ src/main/kotlin/03.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+val regex = """mul\(\d{1,3},\d{1,3}\)""".toRegex()
+val input = File("src/main/resources/input3").readText()
+
+fun thirdA(): Int {
+    return regex.findAll(input)
+        .map { it.value }
+        .map { productFromMatch(it) }
+        .sum()
+}
+
+fun thirdB(): Int {
+    val muls = regex.findAll(input)
+    val doPositions = """do\(\)""".toRegex().findAll(input).map { it.range.first }
+    val dontPositions = """don't\(\)""".toRegex().findAll(input).map { it.range.first }
+
+    return muls.filter { (lastPosition(it.range.first, doPositions) >= lastPosition(it.range.first, dontPositions)) }
+        .map { productFromMatch(it.value) }
+        .sum()
+}
+
+private fun productFromMatch(match: String): Int {
+    return match
+        .removePrefix("mul(")
+        .removeSuffix(")")
+        .split(",")
+        .let { (a, b) -> a.toInt() * b.toInt() }
+}
+
+private fun lastPosition(toCheck: Int, positions: Sequence<Int>): Int {
+    return positions.filter { it < toCheck }.maxOrNull() ?: 0;
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main() {
     val start = java.util.Date().time
-    println(secondB())
+    println(thirdB())
     val end = java.util.Date().time
     println("Took ${end - start}ms.")
 }
